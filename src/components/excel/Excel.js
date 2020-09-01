afae9f3309753ecd19971766a810d9ff534f0ca4
M1ndForce <legendarynorb@gmail.com>
2020-09-01 16:24:11 +0300

    Base structure framework

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,27 @@ export class Excel {
         this.$el = document.querySelector(selector)
         this.components = options.components || []
     }
+
+    getRoot() {
+        const $root = document.createElement('div')
+        $root.classList.add('excel')
+
+        this.components.forEach(Component => {
+            const $el = document.createElement('div')
+            $el.classList.add(Component.className)
+            const component = new Component($el)
+            $el.innerHTML = component.toHTML()
+            $root.append($el)
+        })
+        return $root
+    }
+
+    render() {
+        console.log(this.$el)
+        // // afterbegin, afterend, beforeend, beforebegin
+        // // this.$el.insertAdjacentHTML('afterbegin', '<h1>Test</h1>')
+        // const  node = document.createElement('h1')
+        // node.textContent = 'TEST'
+        this.$el.append(this.getRoot())
+    }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,7 +1,7 @@
 import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
-    // Повертає шаблон компонента
+    // повернення шаблону компонента
     toHTML() {
         return ''
     }
