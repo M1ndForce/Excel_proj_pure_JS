ccc4e54383826a4ebe00736e60d54c3fa66c2a94
M1ndForce <legendarynorb@gmail.com>
2020-09-01 20:46:30 +0300

    Working config v 1.0 (framework)

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,29 +1,28 @@
+import {$} from '@core/dom';
+
 export class Excel {
     constructor(selector, options) {
-        this.$el = document.querySelector(selector)
+        this.$el = $(selector)
         this.components = options.components || []
     }
 
     getRoot() {
-        const $root = document.createElement('div')
-        $root.classList.add('excel')
+        const $root = $.create('div', 'excel')
 
-        this.components.forEach(Component => {
-            const $el = document.createElement('div')
-            $el.classList.add(Component.className)
+
+        this.components = this.components.map(Component => {
+            const $el = $.create('div', Component.className)
             const component = new Component($el)
-            $el.innerHTML = component.toHTML()
+            $el.html(component.toHTML())
             $root.append($el)
+            return component
         })
         return $root
     }
 
     render() {
-        console.log(this.$el)
-        // // afterbegin, afterend, beforeend, beforebegin
-        // // this.$el.insertAdjacentHTML('afterbegin', '<h1>Test</h1>')
-        // const  node = document.createElement('h1')
-        // node.textContent = 'TEST'
         this.$el.append(this.getRoot())
+
+        this.components.forEach(component => component.init())
     }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,15 @@
-import {DomListener} from '@core/DomListener';
+import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
+    constructor($root, options = {}) {
+        super($root, options.listeners)
+    }
     // повернення шаблону компонента
     toHTML() {
         return ''
+    }
+
+    init() {
+        this.initDomListeners()
     }
 }
