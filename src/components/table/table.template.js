402bb86f7f52518b286b8f6dfc9eed86e8f8ed97
M1ndForce <legendarynorb@gmail.com>
2020-09-18 17:00:39 +0300

    finished base framework functional

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,26 +3,50 @@ const CODES = {
     Z: 90
 }
 
-
-function createCol() {
+function toCell() {
     return `
-    <div class='column'>
-    A
+    <div class="cell" contenteditable="true"> </div>
+    `
+}
+
+function toColumn(col) {
+    return `
+    <div class='column'>${col}</div>
+    `
+}
+
+function createRow(index, content) {
+    return `
+    <div class='row'>
+    <div class="row-info">${index ? index : ''}</div>
+    <div class="row-data">${content}</div>
     </div>
     `
 }
 
-function createRow() {
-    return `
-    <div class='row'>
-    <div class="row-info"></div>
-    <div class="row-data"></div>
-    </div>
-    `
+function toChar(_, index) {
+    return String.fromCharCode(CODES.A + index)
 }
 
 export function createTable(rowsCount = 15) {
-    const colsCount = CODES.Z - CODES.A
+    const colsCount = CODES.Z - CODES.A + 1
+    const rows = []
 
-    return '<h1>TABLE</h1>'
+    const cols = new Array(colsCount)
+        .fill('')
+        .map(toChar)
+        .map(toColumn)
+        .join('')
+
+    rows.push(createRow(null, cols))
+
+    for (let i = 0; i < rowsCount; i++) {
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toCell)
+            .join('')
+        rows.push(createRow(i+1, cells))
+    }
+
+    return rows.join('')
 }
