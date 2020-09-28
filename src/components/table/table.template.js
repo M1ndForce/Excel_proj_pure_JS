7605b404737e75aaa716b434e14e7bec66295e7e
M1ndForce <legendarynorb@gmail.com>
2020-09-28 16:52:32 +0300

    resize start

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,15 +3,15 @@ const CODES = {
     Z: 90
 }
 
-function toCell() {
+function toCell(_, col) {
     return `
-    <div class="cell" contenteditable="true"> </div>
+    <div class="cell" contenteditable="true" data-col="${col}"> </div>
     `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
     return `
-     <div class='column' data-type="resizable">
+     <div class='column' data-type="resizable" data-col="${index}">
       ${col}
       <div class="col-resize" data-resize='col'></div>
     </div>
